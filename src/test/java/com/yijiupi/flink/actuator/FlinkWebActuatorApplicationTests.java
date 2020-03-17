d0930e38a5b88a294945434cbd4d35e95c20f660
wenbao <1010467452@qq.com>
2020-03-17 17:46:29 +0800

    初始化项目

---
+++ src/test/java/com/yijiupi/flink/actuator/FlinkWebActuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yijiupi.flink.actuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlinkWebActuatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
